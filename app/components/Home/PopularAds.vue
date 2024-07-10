073918449514148d3f548d15a111d1807f727c88
AsirMahmud <email@asirmahmuhhdd@gamil.com>
2024-07-10 07:40:02 +0600

    Some changes

--- app/components/Home/PopularAds.vue
+++ app/components/Home/PopularAds.vue
@@ -1,23 +1,22 @@
 <script setup>
-function getQueryVariable(variable = 'v', url) {
-    var query = url.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (decodeURIComponent(pair[0]) == variable) {
-            return decodeURIComponent(pair[1]);
-        }
+function getYouTubeEmbedUrl(url) {
+    const parsedUrl = new URL(url);
+    const videoId = parsedUrl.searchParams.get('v');
+    if (videoId) {
+        return `https://www.youtube.com/embed/${videoId}`;
+    } else {
+        console.log('No video ID found in URL:', url);
+        return null;
     }
-    console.log('Query variable %s not found', variable);
 }
-const url = new URL('https://www.youtube.com/watch?v=vP9QpcqzSns')
-console.log(getQueryVariable('v', url));
-getCetagories()
+
+
 
 const store = useUtils()
-const upperLeftAds = await store.getAds('Home Page - Upper Left')
-const upperRightAds = await store.getAds('Home Page - Upper Right')
-
+const bottomLeftAds = await store.getAds('Home Page - Bottom Left')
+const bottomRightAdsUp = await store.getAds('Home Page - Bottom Right Up')
+const bottomRightAdsDown = await store.getAds('Home Page - Bottom Right Down')
+console.log(bottomRightAdsUp.value)
 
 </script>
 
@@ -25,17 +24,29 @@ const upperRightAds = await store.getAds('Home Page - Upper Right')
     <div class="flex flex-col gap-4 p-4">
         <h1 class=" font-bold text-xl">Popular Ads</h1>
         <div class="w-full h-[580px] flex gap-8   ">
-            <div class="border w-full h-full  ">
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/vP9QpcqzSns"
-                    title="YouTube video player" frameborder="0"
+            <div class="border w-full h-full rounded o  ">
+                <iframe v-if="bottomLeftAds.type === 'youtube'" width="100%" height="100%"
+                    :src="getYouTubeEmbedUrl(bottomLeftAds.url)" title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <img v-if="bottomLeftAds.type === 'image'" class="w-full h-full object-cover">
             </div>
             <div class="w-full flex h-full gap-4 flex-col">
                 <div class="border w-full h-[50%]">
+                    <iframe v-if="bottomRightAdsUp.type === 'youtube'" width="100%" height="100%"
+                        :src="getYouTubeEmbedUrl(bottomRightAdsUp.url)" title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <img v-if="bottomRightAdsUp.type === 'image'" class="w-full h-full object-cover">
+
 
                 </div>
                 <div class=" border w-full h-[50%] ">
+                    <iframe v-if="bottomRightAdsUp.type === 'youtube'" width="100%" height="100%"
+                        :src="getYouTubeEmbedUrl(bottomRightAdsDown.url)" title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <img v-if="bottomRightAdsUp.type === 'image'" class="w-full h-full object-cover">
 
                 </div>
             </div>
